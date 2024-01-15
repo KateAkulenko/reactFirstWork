4a2cf5dda2c8fa165d83a615378f56e4bc4a2588
“KateAkulenko” <katyanasoloda@gmail.com>
2024-01-15 21:21:52 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+//*styles
+import styles from './components/Button/Button.module.css';
+//*base
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 
 function App() {
+ 
+  const [name, setName] = useState('Jhon');
+  const [age, setAge] = useState(30);
+  const clickFunction = () => {
+    setName('Willy');
+    setAge(32);
+  };
+
   return (
     <>
       <Header />
       <div style={{ marginTop: 200, padding: 100}}>
         <h2>Content</h2>
         <List />
+      </div>
+      <div style={{ padding: 100}}>
+      <p>{name} has {age} years old</p>
+        <button className={styles.button} onClick={clickFunction}>Change</button>
       </div>
     </>
   );
